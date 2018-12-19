5de3e0375aa7f969bee9c93499dc1eabc1adffeb
huwenlong <lnly1994@163.com>
2018-12-19 10:20:57 +0800

    init project

---
+++ bus-client/src/main/java/com/melo/EurekaConfigBusClientApplication.java
@@ -0,0 +1,21 @@
+package com.melo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EurekaConfigBusClientApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaConfigBusClientApplication.class, args);
+	}
+
+/*	@Bean
+	public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+		PropertySourcesPlaceholderConfigurer c = new PropertySourcesPlaceholderConfigurer();
+		c.setIgnoreUnresolvablePlaceholders(true);
+		return c;
+	}*/
+
+}
